7312019f9f70d6e085af005748373027013ff6ac
Twometer <twometer@outlook.de>
2020-04-07 12:02:56 +0200

    Firmware basics

--- TVRFw/TVRFw.ino
+++ TVRFw/TVRFw.ino
@@ -1,9 +1,23 @@
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);     // Ah yes, debug
+  Serial.println("Twometer VR Firmware v1.0");
+  
+  WiFi.persistent(true);  // Save those credentials
+  WiFi.mode(WIFI_STA);    // Station mode for ESP-firmware glitch prevention
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
 
+  Serial.println("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  Serial.println("Connected");
+
+  // Now, connect to the server
+  // To find it we need some discovery...
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Read accelerometer
+  // Read buttons
+  // Send it to the server
 }
