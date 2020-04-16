1361aa91d6927cf273315bc0b6363b494176fc04
Twometer <twometer@outlook.de>
2020-04-16 21:16:17 +0200

    Add CLI

---
+++ TVRSvc/TVRSvc.CLI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVRSvc.CLI
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            TVRHost vrHost = new TVRHost();
+            vrHost.Start();
+
+            while (true)
+            {
+                var cmd = Console.ReadLine();
+                if (cmd == "help")
+                {
+                    Console.WriteLine("help: Show this help");
+                    Console.WriteLine("stop: Stop the service");
+                }
+                else if (cmd == "stop")
+                {
+                    Console.WriteLine("Shutting down...");
+                    vrHost.Stop();
+                    return;
+                }
+            }
+        }
+    }
+}
