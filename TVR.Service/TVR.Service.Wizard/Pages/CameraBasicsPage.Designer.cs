6580eecab9f16f3f415be9844fd0b002d6a71bd3
Twometer <twometer@outlook.de>
2020-04-23 23:58:36 +0200

    Make design more appealing

--- TVR.Service/TVR.Service.Wizard/Pages/CameraBasicsPage.Designer.cs
+++ TVR.Service/TVR.Service.Wizard/Pages/CameraBasicsPage.Designer.cs
@@ -28,33 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label2
+            // label3
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 43);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(318, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "First, you will have to configure the basic settings for your webcam\r\n";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Configure your camera";
             // 
-            // label1
+            // label4
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(19, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(135, 24);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Camera Basics";
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri Light", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Camera";
             // 
             // CameraBasicsPage
             // 
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
             this.Name = "CameraBasicsPage";
             this.Size = new System.Drawing.Size(579, 345);
             this.ResumeLayout(false);
@@ -64,7 +65,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- TVR.Service/TVR.Service.Wizard/Pages/WelcomePage.Designer.cs
+++ TVR.Service/TVR.Service.Wizard/Pages/WelcomePage.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WelcomePage));
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -35,28 +36,30 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Font = new System.Drawing.Font("Calibri Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 85);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(310, 13);
+            this.label2.Size = new System.Drawing.Size(329, 135);
             this.label2.TabIndex = 3;
-            this.label2.Text = "This setup will guide you through the first setup of Twometer VR.";
+            this.label2.Text = resources.GetString("label2.Text");
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(61, 25);
+            this.label1.Font = new System.Drawing.Font("Calibri Light", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 24);
+            this.label1.Size = new System.Drawing.Size(269, 29);
             this.label1.TabIndex = 2;
-            this.label1.Text = "TwometerVR";
+            this.label1.Text = "Welcome to TwometerVR!";
             // 
             // WelcomePage
             // 
+            this.BackColor = System.Drawing.Color.White;
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "WelcomePage";
-            this.Size = new System.Drawing.Size(377, 280);
+            this.Size = new System.Drawing.Size(543, 382);
             this.ResumeLayout(false);
             this.PerformLayout();
 
