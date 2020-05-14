b3378c2fa9dc0b9ccfd394cbc096a4791dc5f753
Twometer <twometer@outlook.de>
2020-05-14 22:35:18 +0200

    New hardware does not longer require inverted yaw

--- TVR.Service/TVR.Service.Core/Tracking/TrackerManager.cs
+++ TVR.Service/TVR.Service.Core/Tracking/TrackerManager.cs
@@ -52,7 +52,7 @@ namespace TVR.Service.Core.Tracking
 
             // To make stuff complicated, The MPU has its base axis in a different plane than our coordinate system
             // So we have to shift around yaw, pitch and roll here to make it work with SteamVR and TVR
-            controller.Yaw = -yaw;
+            controller.Yaw = yaw;
             controller.Pitch = roll;
             controller.Roll = pitch;
 
