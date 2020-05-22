693de905020dc6501ea54a3f995633bad7de95e6
Twometer <twometer@outlook.de>
2020-05-22 14:07:14 +0200

    Remove pitch invert from config

--- TVR.Service/TVR.Service.Core/Config/TrackerConfig.cs
+++ TVR.Service/TVR.Service.Core/Config/TrackerConfig.cs
@@ -8,10 +8,6 @@ namespace TVR.Service.Core.Config
 {
     public class TrackerConfig
     {
-        public bool LeftInvertPitch { get; set; }
-
-        public bool RightInvertPitch { get; set; }
-
         public double PoseResetDelay { get; set; }
         
         public float ZOffset { get; set; }
