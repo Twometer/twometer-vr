98d9f0eba41300fba290545e460be967ce177a16
Twometer <twometer@outlook.de>
2020-08-20 16:05:34 +0200

    Add quaternion filter to service

---
+++ TVR.Service/TVR.Service.Core/Math/QuaternionFilter.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVR.Service.Core.Math
+{
+    public class QuaternionFilter
+    {
+        private double beta;
+
+        private double deltat = 0.0f, sum = 0.0f;        // integration interval for both filter schemes
+        private long lastUpdate = 0, firstUpdate = 0; // used to calculate integration interval
+        private long Now = 0;
+
+        private const double PI = System.Math.PI;
+
+        private const long TicksPerMicrosecond = TimeSpan.TicksPerMillisecond / 1000;
+
+        private Quaternion _quat = new Quaternion();
+        public Quaternion Quaternion => _quat;
+
+        public QuaternionFilter(double gyroError)
+        {
+            beta = System.Math.Sqrt(3.0f / 4.0f) * System.Math.PI * (gyroError / 180.0f);
+        }
+
+        public void Update(double ax, double ay, double az, double gx, double gy, double gz, double mx, double my, double mz)
+        {
+            double q1 = Quaternion.X, q2 = Quaternion.Y, q3 = Quaternion.Z, q4 = Quaternion.W;   // short name local variable for readability
+            double norm;
+            double hx, hy, _2bx, _2bz;
+            double s1, s2, s3, s4;
+            double qDot1, qDot2, qDot3, qDot4;
+
+            // Auxiliary variables to avoid repeated arithmetic
+            double _2q1mx;
+            double _2q1my;
+            double _2q1mz;
+            double _2q2mx;
+            double _4bx;
+            double _4bz;
+            double _2q1 = 2.0f * q1;
+            double _2q2 = 2.0f * q2;
+            double _2q3 = 2.0f * q3;
+            double _2q4 = 2.0f * q4;
+            double _2q1q3 = 2.0f * q1 * q3;
+            double _2q3q4 = 2.0f * q3 * q4;
+            double q1q1 = q1 * q1;
+            double q1q2 = q1 * q2;
+            double q1q3 = q1 * q3;
+            double q1q4 = q1 * q4;
+            double q2q2 = q2 * q2;
+            double q2q3 = q2 * q3;
+            double q2q4 = q2 * q4;
+            double q3q3 = q3 * q3;
+            double q3q4 = q3 * q4;
+            double q4q4 = q4 * q4;
+            gx *= PI / 180.0f;
+            gy *= PI / 180.0f;
+            gz *= PI / 180.0f;
+            
+            Now = Microseconds();
+            deltat = ((Now - lastUpdate) / 1000000.0); // set integration time by time elapsed since last filter update
+            lastUpdate = Now;
+
+            // Normalise accelerometer measurement
+            norm = System.Math.Sqrt(ax * ax + ay * ay + az * az);
+            if (norm == 0.0f) return; // handle NaN
+            norm = 1.0f / norm;
+            ax *= norm;
+            ay *= norm;
+            az *= norm;
+
+            // Normalise magnetometer measurement
+            norm = System.Math.Sqrt(mx * mx + my * my + mz * mz);
+            if (norm == 0.0f) return; // handle NaN
+            norm = 1.0f / norm;
+            mx *= norm;
+            my *= norm;
+            mz *= norm;
+
+            // Reference direction of Earth's magnetic field
+            _2q1mx = 2.0f * q1 * mx;
+            _2q1my = 2.0f * q1 * my;
+            _2q1mz = 2.0f * q1 * mz;
+            _2q2mx = 2.0f * q2 * mx;
+            hx = mx * q1q1 - _2q1my * q4 + _2q1mz * q3 + mx * q2q2 + _2q2 * my * q3 + _2q2 * mz * q4 - mx * q3q3 - mx * q4q4;
+            hy = _2q1mx * q4 + my * q1q1 - _2q1mz * q2 + _2q2mx * q3 - my * q2q2 + my * q3q3 + _2q3 * mz * q4 - my * q4q4;
+            _2bx = System.Math.Sqrt(hx * hx + hy * hy);
+            _2bz = -_2q1mx * q3 + _2q1my * q2 + mz * q1q1 + _2q2mx * q4 - mz * q2q2 + _2q3 * my * q4 - mz * q3q3 + mz * q4q4;
+            _4bx = 2.0f * _2bx;
+            _4bz = 2.0f * _2bz;
+
+            // Gradient decent algorithm corrective step
+            s1 = -_2q3 * (2.0f * q2q4 - _2q1q3 - ax) + _2q2 * (2.0f * q1q2 + _2q3q4 - ay) - _2bz * q3 * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q4 + _2bz * q2) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q3 * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
+            s2 = _2q4 * (2.0f * q2q4 - _2q1q3 - ax) + _2q1 * (2.0f * q1q2 + _2q3q4 - ay) - 4.0f * q2 * (1.0f - 2.0f * q2q2 - 2.0f * q3q3 - az) + _2bz * q4 * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q3 + _2bz * q1) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q4 - _4bz * q2) * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
+            s3 = -_2q1 * (2.0f * q2q4 - _2q1q3 - ax) + _2q4 * (2.0f * q1q2 + _2q3q4 - ay) - 4.0f * q3 * (1.0f - 2.0f * q2q2 - 2.0f * q3q3 - az) + (-_4bx * q3 - _2bz * q1) * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (_2bx * q2 + _2bz * q4) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + (_2bx * q1 - _4bz * q3) * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
+            s4 = _2q2 * (2.0f * q2q4 - _2q1q3 - ax) + _2q3 * (2.0f * q1q2 + _2q3q4 - ay) + (-_4bx * q4 + _2bz * q2) * (_2bx * (0.5f - q3q3 - q4q4) + _2bz * (q2q4 - q1q3) - mx) + (-_2bx * q1 + _2bz * q3) * (_2bx * (q2q3 - q1q4) + _2bz * (q1q2 + q3q4) - my) + _2bx * q2 * (_2bx * (q1q3 + q2q4) + _2bz * (0.5f - q2q2 - q3q3) - mz);
+            norm = System.Math.Sqrt(s1 * s1 + s2 * s2 + s3 * s3 + s4 * s4);    // normalise step magnitude
+            norm = 1.0f / norm;
+            s1 *= norm;
+            s2 *= norm;
+            s3 *= norm;
+            s4 *= norm;
+
+            // Compute rate of change of quaternion
+            qDot1 = 0.5f * (-q2 * gx - q3 * gy - q4 * gz) - beta * s1;
+            qDot2 = 0.5f * (q1 * gx + q3 * gz - q4 * gy) - beta * s2;
+            qDot3 = 0.5f * (q1 * gy - q2 * gz + q4 * gx) - beta * s3;
+            qDot4 = 0.5f * (q1 * gz + q2 * gy - q3 * gx) - beta * s4;
+
+            // Integrate to yield quaternion
+            q1 += qDot1 * deltat;
+            q2 += qDot2 * deltat;
+            q3 += qDot3 * deltat;
+            q4 += qDot4 * deltat;
+            norm = System.Math.Sqrt(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4);    // normalise quaternion
+            norm = 1.0f / norm;
+            _quat.X = -(float)(q2 * norm);
+            _quat.Y = (float)(q1 * norm);
+            _quat.Z = (float)(q3 * norm);
+            _quat.W = (float)(q4 * norm);
+        }
+
+        private long Microseconds()
+        {
+            return DateTime.Now.Ticks / TicksPerMicrosecond;
+        }
+
+    }
+}
