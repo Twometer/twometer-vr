95fdc83ab0e39eccc98dd7021d5385c08c79ddb7
Twometer <twometer@outlook.de>
2020-09-30 15:20:51 +0200

    Add test project for experimenting with tracking

---
+++ nextgentrackingdemo/nextgentrackingdemo/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace nextgentrackingdemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.glControl1 = new OpenTK.GLControl();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Begin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox1.Location = new System.Drawing.Point(13, 110);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(512, 347);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox2.Location = new System.Drawing.Point(557, 110);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(512, 347);
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Raw";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(554, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Processed";
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.glControl1.Location = new System.Drawing.Point(12, 472);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(1057, 413);
+            this.glControl1.TabIndex = 6;
+            this.glControl1.VSync = false;
+            this.glControl1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.glControl1_Scroll);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            this.glControl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseDown);
+            this.glControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseMove);
+            this.glControl1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(17, 477);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1085, 897);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.glControl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ nextgentrackingdemo/nextgentrackingdemo/Form1.cs
@@ -0,0 +1,224 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TVR.Service.Core.IO;
+using TVector3 = TVR.Service.Core.Math.Vector3;
+using TVR.Service.Core.Tracking;
+using TVR.Service.Core.Video;
+using System.Security.Permissions;
+
+namespace nextgentrackingdemo
+{
+    public partial class Form1 : Form
+    {
+        private volatile bool running = true;
+
+        private Vector3 position = new Vector3(1, 2, 0);
+
+        public Form1()
+        {
+            InitializeComponent();
+            glControl1.MouseWheel += GlControl1_MouseWheel;
+        }
+
+        private async void UpdateLoop()
+        {
+            var conf = ConfigIO.LoadUserConfig();
+            var cam = new Camera(conf.CameraInfo);
+            var tracker = new TrackingManager(conf);
+
+            while (running)
+                if (cam.Update())
+                {
+                    await tracker.UpdateVideo(cam.HsvFrame, cam.FrameBrightness);
+                    imageBox1.Image = cam.Frame;
+                    imageBox2.Image = tracker.Trackers[1].Frame;
+                    AdvancedTracking(tracker.Trackers[1].TrackedController.Position);
+                }
+        }
+
+        private void AdvancedTracking(TVector3 vec)
+        {
+            position = new Vector3(vec.X, vec.Y, vec.Z);
+            glControl1.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateLoop();
+        }
+
+        float yaw = -1.8f;
+        float pitch = 0.4f;
+        float zoom = 4.5f;
+
+        float x = 0;
+        float y = 0;
+        float z = 0;
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            GL.LineWidth(1.5f);
+            GL.ClearColor(1, 1, 1, 1);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            var perspective = Matrix4.CreatePerspectiveFieldOfView(1.04f, glControl1.Width / (float)glControl1.Height, 0.5f, 100); // Setup Perspective
+
+            var l = Math.Cos(pitch);
+
+            var lookat = Matrix4.LookAt((float)(Math.Cos(yaw) * l) * zoom + x, (float)Math.Sin(pitch) * zoom + y, (float)(Math.Sin(-yaw) * l) * zoom + z, x, y, z, 0, 1, 0); // Setup camera
+
+            GL.MatrixMode(MatrixMode.Projection); // Load Perspective
+            GL.LoadIdentity();
+            GL.LoadMatrix(ref perspective);
+            GL.MatrixMode(MatrixMode.Modelview); // Load Camera
+            GL.LoadIdentity();
+            GL.LoadMatrix(ref lookat);
+            GL.Viewport(0, 0, glControl1.Width, glControl1.Height); // Size of window
+            GL.Enable(EnableCap.DepthTest); // Enable correct Z Drawings
+            GL.DepthFunc(DepthFunction.Less); // Enable correct Z Drawings
+
+            GL.Color4(Color.Gray);
+            DrawCross(0, 0, 0, 10);
+
+            GL.Color4(Color.Blue);
+            DrawUnits(0, 0, 1, 10);
+            DrawCross(0, 0, 10, 0.5f);
+            GL.Color4(Color.Green);
+            DrawUnits(0, 1, 0, 10);
+            DrawCross(0, 10, 0, 0.5f);
+            GL.Color4(Color.Red);
+            DrawUnits(1, 0, 0, 10);
+            DrawCross(10, 0, 0, 0.5f);
+
+            GL.Color4(Color.Blue);
+            DrawCross(position.X, position.Y, position.Z, 1.0f);
+
+            label3.Text = yaw + " " + pitch + " " + zoom;
+
+            glControl1.SwapBuffers();
+        }
+
+        private void DrawCross(float x, float y, float z, float size)
+        {
+            GL.Begin(PrimitiveType.Lines);
+            GL.Vertex3(-size + x, y, z);
+            GL.Vertex3(size + x, y, z);
+            GL.End();
+
+            GL.Begin(PrimitiveType.Lines);
+            GL.Vertex3(x, -size + y, z);
+            GL.Vertex3(x, size + y, z);
+            GL.End();
+
+            GL.Begin(PrimitiveType.Lines);
+            GL.Vertex3(x, y, -size + z);
+            GL.Vertex3(x, y, size + z);
+            GL.End();
+        }
+
+        private void DrawLine(float x0, float y0, float z0, float x1, float y1, float z1)
+        {
+            GL.Begin(PrimitiveType.Lines);
+            GL.Vertex3(x0, y0, z0);
+            GL.Vertex3(x1, y1, z1);
+            GL.End();
+        }
+
+        private void DrawUnits(float x, float y, float z, float d)
+        {
+            float ox = 0;
+            float oy = 0;
+            float oz = 0;
+
+            float yd = y == 0 ? 0.2f : 0;
+            float zd = y != 0 ? 0.2f : 0;
+
+            for (var i = 0; i < d; i++)
+            {
+                ox += x;
+                oy += y;
+                oz += z;
+                GL.Begin(PrimitiveType.Lines);
+                GL.Vertex3(ox - zd, oy - yd, oz);
+                GL.Vertex3(ox + zd, oy + yd, oz);
+                GL.End();
+            }
+            ox = 0; oy = 0; oz = 0;
+            for (var i = 0; i < d; i++)
+            {
+                ox -= x;
+                oy -= y;
+                oz -= z;
+                GL.Begin(PrimitiveType.Lines);
+                GL.Vertex3(ox - zd, oy - yd, oz);
+                GL.Vertex3(ox + zd, oy + yd, oz);
+                GL.End();
+            }
+        }
+
+        bool down = false;
+
+        int px = 0;
+        int py = 0;
+
+        private void glControl1_MouseDown(object sender, MouseEventArgs e)
+        {
+            down = true;
+        }
+
+        private void glControl1_MouseUp(object sender, MouseEventArgs e)
+        {
+            down = false;
+        }
+
+        private void glControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+
+            glControl1.Invalidate();
+
+            if (down)
+            {
+                var dx = e.X - px;
+                var dy = e.Y - py;
+
+                yaw -= dx * 0.01f;
+                pitch += dy * 0.01f;
+
+                pitch = (float)MathHelper.Clamp(pitch, -Math.PI / 2 + 0.05, Math.PI / 2 - 0.05);
+
+            }
+
+            px = e.X;
+            py = e.Y;
+        }
+
+        private void GlControl1_MouseWheel(object sender, MouseEventArgs e)
+        {
+
+            zoom -= Math.Sign(e.Delta) * 0.25f;
+            zoom = MathHelper.Clamp(zoom, 2.0f, 10000.0f);
+            glControl1.Invalidate();
+        }
+
+        private void glControl1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
