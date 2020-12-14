0c603183267ba1a10297df270418e1292bd6f556
Twometer <twometer@outlook.de>
2020-12-15 00:58:07 +0100

    Service: Simple config provider

---
+++ TVR.Service/TVR.Service.Core/IO/SimpleConfigProvider.cs
@@ -0,0 +1,21 @@
+﻿using TVR.Service.Core.Model;
+
+namespace TVR.Service.Core.IO
+{
+    public class SimpleConfigProvider : IConfigProvider
+    {
+        public UserConfig UserConfig { get; }
+
+        public VideoSourceConfig VideoSourceConfig { get; }
+
+        public SimpleConfigProvider(UserConfig userConfig, VideoSourceConfig videoSourceConfig)
+        {
+            UserConfig = userConfig;
+            VideoSourceConfig = videoSourceConfig;
+        }
+
+        public void Load()
+        {
+        }
+    }
+}
