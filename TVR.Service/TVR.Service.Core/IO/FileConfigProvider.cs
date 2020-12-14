ddec13f02c4bdf415354e8735f13fe4c5b115028
Twometer <twometer@outlook.de>
2020-12-15 00:57:20 +0100

    Service: Config file loader

--- TVR.Service/TVR.Service.Core/IO/FileConfigProvider.cs
+++ TVR.Service/TVR.Service.Core/IO/FileConfigProvider.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using TVR.Service.Core.Model;
+using YamlDotNet.Serialization;
 
 namespace TVR.Service.Core.IO
 {
@@ -9,9 +11,36 @@ namespace TVR.Service.Core.IO
 
         public VideoSourceConfig VideoSourceConfig { get; private set; }
 
+        private readonly Deserializer deserializer = new Deserializer();
+
         public void Load()
         {
-            throw new NotImplementedException();
+            var baseDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "TwometerVR");
+            if (!Directory.Exists(baseDir))
+                Directory.CreateDirectory(baseDir);
+
+            LoadUserConfig(baseDir);
+            LoadVideoSourceConfig(baseDir);
+        }
+
+        private void LoadUserConfig(string baseDir)
+        {
+            var configFile = Path.Combine(baseDir, "Config.yml");
+            if (!File.Exists(configFile))
+            {
+                throw new FileNotFoundException($"User config file {configFile} does not exist");
+            }
+            UserConfig = deserializer.Deserialize<UserConfig>(File.ReadAllText(configFile));
+        }
+
+        private void LoadVideoSourceConfig(string baseDir)
+        {
+            var videoSourceConfigFile = Path.Combine(baseDir, $"{UserConfig.Hardware.VideoSource}.yml");
+            if (!File.Exists(videoSourceConfigFile))
+            {
+                throw new FileNotFoundException($"Config file for video source '{UserConfig.Hardware.VideoSource}' not found at {videoSourceConfigFile}");
+            }
+            VideoSourceConfig = deserializer.Deserialize<VideoSourceConfig>(File.ReadAllText(videoSourceConfigFile));
         }
     }
 }
